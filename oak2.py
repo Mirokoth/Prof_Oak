776b4fb479a5b08bffe86446c517386a8e55e153
Rhyse Allan <mail@rhy.se>
2016-07-14 15:18:30 +1000

    !roles and welcom
    !roles now accepts plain txt
    accepts first two char of team name or colour
    also added admin easter egg
    
    Now has welcome message for new users. Also PM's them the help command

--- oak2.py
+++ oak2.py
@@ -9,9 +9,11 @@ import discord
 import requests
 from bs4 import BeautifulSoup
 from discord import role
-
+import pyowm
 
 client = discord.Client()
+
+_roles = []
 
 @client.event
 async def on_ready():
@@ -24,23 +26,28 @@ async def on_ready():
     for server in client.servers:
         print(server.name + ' (' + server.id + ')')
         print('------')
+        """
     print('Roles')
     print('------')
-    roles = []
     for role in server.roles:
+        _roles.append(role)
         print(role.name + ' (' + role.id + ')')
-
+    print(_roles)
+        """
 
 @client.event
 async def on_member_join(member):
-    print("new {}".format(member))
-    await client.send_message(message.server, 'Welcome {}'.format(member))
-    await client.send_message(message.member, 'Welcome {}!\nTo get started type **!help**'.format(member))
+    print("new user: {}".format(member))
+    _user = str(member).split('#')
+    _user = _user[0]
+    await client.send_message(member.server, 'Welcome {}'.format(_user))
+    await client.send_message(member, 'Welcome {}!\nTo get started type **!help**'.format(_user))
 
 @client.event
 async def on_message(message):
+
     if message.content.startswith('!help'):
-        _help = '**Assigning team:**\n!role @teamname\n\n**Finding a Pokemon:**\n!find Pokemon_Name\n\n**Server status:**\n!status\n\nMore to come! If you have any feature requests message @Mirokoth'
+        _help = '**Assigning team:**\n!role teamname or !role teamcolour\n\n**Finding a Pokemon:**\n!find Pokemon_Name\n\n**Server status:**\n!status\n\n**Current Canberra Temperature:**\n!temp\n\nMore to come! If you have any feature requests message @Mirokoth'
         await client.send_message(message.channel, '{}'.format(_help))
 
     if message.content.startswith('!find'):
@@ -91,33 +98,6 @@ async def on_message(message):
         if firstRole:
             await client.send_message(message.channel, "Role '" + firstRole.name + "' ID: " + firstRole.id)
 
-
-    if message.content.startswith('!Role') or message.content.startswith('!role'):
-        _tmp_count = 0
-        for role in message.role_mentions:
-            if 'Admin' in role.name:
-                _tmp_count =+ 5
-                await client.send_message(message.channel, 'http://i.imgur.com/MqxSRTm.jpg')
-
-        #for role in message.author.roles:
-            #if 'Mystic' in role.name:
-                #_tmp_count =+ 1
-            #elif 'Valor' in role.name:
-                #_tmp_count =+ 1
-            #elif 'Instinct' in role.name:
-                #_tmp_count =+ 1
-
-        if _tmp_count >= 5:
-                await client.send_message(message.channel, 'Nice Try')
-        if _tmp_count == 0:
-                await client.add_roles(message.author, server.roles[1])
-                #await client.add_roles(message.author, message.role_mentions[0])
-                await client.send_message(message.channel, 'Welcome to team {}!'.format(message.role_mentions[0]))
-        else:
-            print(_tmp_count)
-            await client.send_message(message.channel, 'It looks like you already have a team.\nIf you think this is an error please let us know')
-
-
     if message.content.startswith('!status'):
             await client.send_message(message.channel, 'This can sometimes take a while...')
 
@@ -135,12 +115,56 @@ async def on_message(message):
                 await client.send_message(message.channel, 'Australian server: OFFLINE ')
                 print('server offline')
 
-    if message.content.startswith('!test'):
-        await client.send_message(message.server, 'Welcome ')
-        #for role in server.roles:
-            #await client.send_message(message.channel, '{}'.format(role))
+    if message.content.startswith('!temp'):
+        """ Weather Details"""
+        owm = pyowm.OWM('c70f0e635539159ab87dc1b61f812d5d')
+        _weather_location = owm.weather_at_place('Canberra,AU')
+        _observation = _weather_location.get_weather()
+        _observation = _observation.get_temperature('celsius')
+        await client.send_message(message.channel, 'It is currently {}°C'.format(_observation['temp']))
 
-    if message.content.startswith('!quit'):
+
+    if message.content.startswith('!role'):
+        _term = message.content.split(' ')
+        _tmp_count = 0
+
+        for role in message.author.roles:
+            if 'Mystic' in role.name:
+                _tmp_count =+ 1
+            elif 'Valor' in role.name:
+                _tmp_count =+ 1
+            elif 'Instinct' in role.name:
+                _tmp_count =+ 1
+
+        if 'ADMIN' in _term[1].upper():
+            await client.send_message(message.channel, 'http://i.imgur.com/MqxSRTm.jpg')
+            _tmp_count =+ 5
+
+        if _tmp_count == 0:
+            if 'MY' in _term[1].upper() or 'BL' in _term[1].upper():
+                for role in _roles:
+                    if 'MYSTIC' in str(role.name.upper()) and 'ADMIN' not in str(role.name.upper()):
+                        await client.add_roles(message.author, role)
+                        await client.send_message(message.channel, 'Welcome to team {}!'.format(role.name))
+            if 'VA' in _term[1].upper() or 'RE' in _term[1].upper():
+                for role in _roles:
+                    if 'VALOR' in str(role.name.upper()) and 'ADMIN' not in str(role.name.upper()):
+                        await client.add_roles(message.author, role)
+                        await client.send_message(message.channel, 'Welcome to team {}!'.format(role.name))
+            if 'IN' in _term[1].upper() or 'YE' in _term[1].upper():
+                for role in _roles:
+                    if 'INSTINCT' in str(role.name.upper()) and 'ADMIN' not in str(role.name.upper()):
+                        await client.add_roles(message.author, role)
+                        await client.send_message(message.channel, 'Welcome to team {}!'.format(role.name))
+        if _tmp_count >= 5:
+            await client.send_message(message.channel, 'Nice try ;)')
+        else:
+            await client.send_message(message.channel, 'It looks like you already have a team.\nIf you think this is an error please let us know')
+
+
+
+
+    if message.content.startswith('!die'):
         await quit()
 
 
